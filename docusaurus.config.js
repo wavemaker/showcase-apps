0d36a3f939add6244fecb7f6ec18b950587456c1
abhilashk <abhilashk@PRINHYLTPAP1089.local>
2020-02-11 17:42:15 +0530

    changed title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Showcase_Apps',
+      title: 'Showcase-Apps',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
